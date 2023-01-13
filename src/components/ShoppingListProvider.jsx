f6a044f41c1829b72cb46139db15b2404f5cc9aa
Tanner Richards <Tanner.richards990@gmail.com>
2023-01-12 17:29:26 -0800

    built out shoppinglistprovider file for managing state changes with the reducer

--- src/components/ShoppingListProvider.jsx
+++ src/components/ShoppingListProvider.jsx
@@ -1,8 +1,23 @@
 import { createContext, useReducer } from 'react';
 
 import {
-  initalState,
+  initialState,
   reducer,
 } from './reducers/shopping-list-reducer';
 
 import { reducerLogger } from './reducers/reducer-logger';
+
+export const Context = createContext({
+  state: initialState(),
+  reducer,
+});
+
+export const PostListProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducerLogger(reducer), initialState());
+
+  const contextObj = { state, dispatch };
+
+  return (
+    <Context.Provider value={contextObj}>{children}</Context.Provider>
+  );
+};
