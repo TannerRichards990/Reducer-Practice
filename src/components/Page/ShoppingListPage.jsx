07c9d456063c0426c5b15c03dc860fde2de47016
Tanner Richards <Tanner.richards990@gmail.com>
2023-01-16 18:10:21 -0800

    This code is a React component that renders a form to create a new shopping list item and a list of existing items, it allows the user to create new items, change the status of the items and update the database. It also handles the loading state of the component, showing a loading message while the data is being fetched.

--- src/components/Page/ShoppingListPage.jsx
+++ src/components/Page/ShoppingListPage.jsx
@@ -1,13 +1,109 @@
+import ShoppingPostList from './ShoppingPostList';
+import { Context } from '../ShoppingListProvider.jsx';
+import { useContext, useEffect } from 'react';
+import { createShoppingListItem } from '../../services/shopping-list-items';
 
-
-
-
-
-
+import {
+  shoppingListCandidateBodyChanged,
+  shoppingListSeenChanged,
+} from '../actions/shopping-list-actions';
+import ShoppingListForm from './ShoppingListForm';
+import { 
+  getPostsEffect,
+  updateShoppingListItemEffect, 
+} from '../effects/shopping-list-effects';
 
 
 export default function ShoppingListPage() {
-  return <section>
-    <h1>My Shopping List</h1>
-  </section>;
+  const { state, dispatch } = useContext(Context);
+  useEffect(() => {
+    getPostsEffect(dispatch);
+  }, []);
+
+  const handleSeenChanged = (postId, seen) => {
+    dispatch(shoppingListSeenChanged(postId, seen));
+    updateShoppingListItemEffect(dispatch, postId, { seen });
+  };
+
+  return (
+    <>
+      <ShoppingListForm
+        body={state.shoppingCandidateBody}
+        onBodyChanged={(body) => {
+          dispatch(shoppingListCandidateBodyChanged(body));
+        }}
+        onSubmit={async (body) => {
+          await createShoppingListItem(body);
+          getPostsEffect(dispatch);
+          dispatch(shoppingListCandidateBodyChanged(''));
+        }}
+      />
+      {state.loadingMode === 'Loading' ? (
+        <div>Loading Shopping List...</div>
+      ) : (
+        <ShoppingPostList
+          shoppingList={state.shoppingList}
+          handleSeenChangedByPostId={(postId, seen) => {
+            handleSeenChanged(postId, seen);
+          }}
+        />
+      )}
+    </>
+  );
 }
+
+
+
+// import ShoppingPostList from './ShoppingPostList';
+// import { Context } from '../ShoppingListProvider.jsx';
+// import { useContext, uesEffect, useEffect } from 'react';
+// import { createShoppingListItem } from '../../services/shopping-list-items';
+
+// import {
+//   shoppingListCandidateBodyChanged,
+//   shoppingListSeenChanged,
+// } from '../actions/shopping-list-actions';
+// import ShoppingListForm from './ShoppingListForm';
+// import { 
+//   getPostEffect,
+//   updateShoppingListItemEffect, 
+// } from '../../effects/shopping-list-effects';
+
+
+// export default function ShoppingListPage() {
+//   const { state, dispatch } = useContext(Context);
+//   useEffect(() => {
+//     getPostEffect(dispatch);
+//   }, []);
+
+//   const handleSeenChanged = (postId, seen) => {
+//     dispatch(shoppingListSeenChanged(postId, seen));
+//     updateShoppingListItemEffect(dispatch, postId, { seen });
+//   };
+
+//   return (
+//     <>
+//       <ShoppingListForm
+//         body={state.shoppingCandiateBody}
+//         onBodyChanged={(body) => {
+//           dispatch(shoppingListCandidateBodyChanged(body));
+//         }}
+//         onSubmit={async (body) => {
+//           await createShoppingListItem(body);
+//           getPostEffect(dispatch);
+//           dispatch(shoppingListCandidateBodyChanged(''));
+//         }}
+//       />
+//       {state.loadingMode === 'Loading' ? (
+//         <div>Loading Shopping List...</div>
+//       ) : (
+//         <ShoppingPostList
+//           shoppingList={state.shoppingList}
+//           handleSeenChangedByPostId={(postId, seen) => {
+//             handleSeenChanged(postId, seen);
+//           }}
+//         />
+//       )}
+//     </>
+//   );
+// }
