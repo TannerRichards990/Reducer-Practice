82a3341f7f3436eeef979a3852d04fa2ebbfde0d
Tanner Richards <Tanner.richards990@gmail.com>
2023-01-16 17:55:11 -0800

    The shoppingpostlist component renders an ordered list that maps over an array of post and creates a component for each post, which then allows the seen state of the post to change and update accordingly with the parent component.

---
+++ src/components/Page/ShoppingPostList.jsx
@@ -0,0 +1,21 @@
+import ShoppingPost from './ShoppingPost';
+
+export default function ShoppingPostList({
+  shoppingList,
+  handleSeenChangedByPostId,
+}) {
+  return (
+    <ol>
+      {shoppingList.map((post) => (
+        <li key={post.id}>
+          <ShoppingPost
+            post={post}
+            handleSeenChanged={(seen) => {
+              handleSeenChangedByPostId(post.id, seen);
+            }}
+          />
+        </li>
+      ))}
+    </ol>
+  );
+}
